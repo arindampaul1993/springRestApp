a6a8fe5fee560d46570b971077ac0de543c52b04
Shabiq Hazarika <hazarika.shabiq@gmail.com>
2017-12-28 00:21:32 +0530

    First commit

---
+++ src/main/java/com/paul/springmvc/service/UserService.java
@@ -0,0 +1,27 @@
+package com.paul.springmvc.service;
+
+import java.util.List;
+
+import com.paul.springmvc.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
